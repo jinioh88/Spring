6fb014effe262cb517e404bd1a18a003904eff62
OHSEJIN <jinio88@gmail.com>
2018-04-16 20:26:06 +0900

    aop

--- aop2/src/main/java/ex1/MyAspect.java
+++ aop2/src/main/java/ex1/MyAspect.java
@@ -1,3 +1,6 @@
+/*
+ * AOP @Before 적용
+ */
 package ex1;
 
 import org.aspectj.lang.JoinPoint;
@@ -7,6 +10,7 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class MyAspect {
 	@Before("execution(public void ex1.Boy.runSomething ())")
+	//@Before("execution(*.runSomething ())")
 	public void before(JoinPoint joinPoint) {
 		System.out.println("얼굴확인 : 문 개방!");
 	}
