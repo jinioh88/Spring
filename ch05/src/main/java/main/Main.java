8b5b6a63b6ca12a0017b5715f515967e2156537d
오세진 <jinioh88@gmail.com>
2018-04-18 13:19:49 +0900

    xml 설정에서 자바 설정 임포트

--- ch05/src/main/java/main/Main.java
+++ ch05/src/main/java/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import config.JavaMainConf;
 import spring.MemberInfoPrinter;
@@ -12,8 +13,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(JavaMainConf.class); 
-				MemberRegisterService regSvc = 
+//		ApplicationContext ctx = new AnnotationConfigApplicationContext(JavaMainConf.class); 
+		ApplicationContext ctx = new GenericXmlApplicationContext("classpath:main-conf.xml");
+		MemberRegisterService regSvc = 
 				ctx.getBean("memberRegSvc", MemberRegisterService.class);
 		MemberInfoPrinter infoPrinter = 
 				ctx.getBean("infoPrinter", MemberInfoPrinter.class);
