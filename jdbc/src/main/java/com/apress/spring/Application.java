cbf420e0654f203c04eddeb6563664a3bee367bf
OHSEJIN <jinio88@gmail.com>
2018-04-27 22:50:52 +0900

    도메인, 서비스 생성

--- jdbc/src/main/java/com/apress/spring/Application.java
+++ jdbc/src/main/java/com/apress/spring/Application.java
@@ -1,12 +1,32 @@
 package com.apress.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import com.apress.spring.service.JournalService;
 
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+	@Autowired
+	JournalService service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("--데이터 생성--");
+		service.insertData();
+		log.info("--findAll()--");
+		service.findAll().forEach(entry->log.info(entry.toString()));
+		
+	}
+	
 }
