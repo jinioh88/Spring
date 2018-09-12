6d7ca0a5de4cc72b8534ed342eb5c402af0ec4f2
오세진 <jinioh88@gmail.com>
2018-09-12 13:07:31 +0900

    싱글턴패턴

---
+++ DesignPattern/src/singleton/Client.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class Client {
+    public static void main(String[] args) {
+//        Singleton s = new Singleton();
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        s1 = null;
+        s2 = null;
+        s3 = null;
+    }
+}
---
+++ DesignPattern/src/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package singleton;
+
+public class Singleton {
+    static Singleton singletonObject;
+
+    private Singleton() {};
+
+    public static Singleton getInstance() {
+        if(singletonObject==null)
+            singletonObject = new Singleton();
+        return singletonObject;
+    }
+
+}
