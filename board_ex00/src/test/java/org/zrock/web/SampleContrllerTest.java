fac9bc819f801b97eb101e321424859772a47907
ohsejin <jinioh88@gmail.com>
2018-09-25 09:54:16 +0900

    MockMvc

---
+++ board_ex00/src/test/java/org/zrock/web/SampleContrllerTest.java
@@ -0,0 +1,45 @@
+package org.zrock.web;
+
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class SampleContrllerTest {
+	private static final Logger logger = LoggerFactory.getLogger(SampleContrllerTest.class);
+	
+	@Inject
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+		logger.info("setup,,,,");
+	}
+	
+	@Test
+	public void testDoA() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/doA"));
+	}
+}
