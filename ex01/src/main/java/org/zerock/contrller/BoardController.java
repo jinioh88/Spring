fc148c248294b159654a818b88701daa0eea0d0a
ohsejin <jinioh88@gmail.com>
2018-09-26 16:47:09 +0900

    컨트롤러 뷰

---
+++ ex01/src/main/java/org/zerock/contrller/BoardController.java
@@ -0,0 +1,43 @@
+package org.zerock.contrller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Inject
+	private BoardService service;
+	
+	@RequestMapping(value="/register",method=RequestMethod.GET)
+	public void registerGet(BoardVO board, Model model) throws Exception {
+		logger.info("register get...");
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String registerPost(BoardVO board, RedirectAttributes rttr) throws Exception {
+		logger.info("register post...");
+		logger.info(board.toString());
+		
+		service.registrer(board);
+		rttr.addFlashAttribute("msg","success");
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value="/listAll", method=RequestMethod.GET)
+	public void listAll(Model model) throws Exception {
+		logger.info("show all list..");
+	}
+	
+}
