680805f0c43f26ae2fc10001eb7653c2c8daf823
ohsejin <jinioh88@gmail.com>
2018-09-26 17:32:13 +0900

    조회

--- ex01/src/main/java/org/zerock/contrller/BoardController.java
+++ ex01/src/main/java/org/zerock/contrller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
@@ -39,6 +40,11 @@ public class BoardController {
 	public void listAll(Model model) throws Exception {
 		logger.info("show all list..");
 		model.addAttribute("list",service.listAll());
+	}
+	
+	@RequestMapping(value="/read", method=RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception {
+		model.addAttribute(service.read(bno));
 	}
 	
 }
