ff3363046133e806a1a90b80d69b96ca89d98b3e
ohsejin <jinioh88@gmail.com>
2018-09-30 22:13:14 +0900

    댓글 페이징처리

--- ex01/src/main/java/org/zerock/service/ReplyService.java
+++ ex01/src/main/java/org/zerock/service/ReplyService.java
@@ -2,6 +2,7 @@ package org.zerock.service;
 
 import java.util.List;
 
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 
 public interface ReplyService {
@@ -9,4 +10,6 @@ public interface ReplyService {
 	public List<ReplyVO> listReply(Integer bno) throws Exception;
 	public void modifyReply(ReplyVO vo) throws Exception;
 	public void removeReply(Integer rno) throws Exception;
+	public List<ReplyVO> listReplyPage(Integer bno, Criteria cri) throws Exception;
+	public int count(Integer bno) throws Exception;
 }
